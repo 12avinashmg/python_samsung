4b69c7b591f8e94d8fa2edd26443acc49a17f0b7
M G Avinash <162713420+12avinashmg@users.noreply.github.com>
2024-12-17 19:58:54 +0530

    Delete day-4 directory

--- day-4/digit.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-input_number = int(sys.argv[1])
-print(f'User given number is {input_number}')
-
-digits = [int(digit) for digit in sys.argv[1]]
-print(f'Sum of the digits of {input_number} is {sum(digits)}')
