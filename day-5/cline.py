15f531ca4c1af092d6510526d8a7bc42b23e6f67
M G Avinash <162713420+12avinashmg@users.noreply.github.com>
2024-12-17 19:59:18 +0530

    Delete day-5 directory

--- day-5/cline.py
+++
@@ -1,8 +0,0 @@
-
-import sys
-
-input_number = int(sys.argv[1])
-
-for i in range(1, 21):
-    #print(f'{input_number} * {i} = {input_number * i}')
-    print('%d * %02d = %03d'%(input_number, i, input_number*i))
