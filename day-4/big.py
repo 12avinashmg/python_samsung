822f7de249cf796b63a5e3a601d38be4ec81c392
12avinashmg <avinashmg000@gmail.com>
2024-12-18 09:50:29 +0530

    python_samsung

---
+++ day-4/big.py
@@ -0,0 +1,10 @@
+num1=int(input("enter nuber"))
+num2=int(input("enter nuber"))
+num3=int(input("enter nuber"))
+
+if num1 > num2 and num1 > num3:
+	 print num1 is big
+elif num2 > num3:
+	print num2 is big
+else:
+	print num3 is big
